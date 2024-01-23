43c9909b88873c4868d890254b885d911a6666d1
dmrozik87 <dmrozik87@gmail.com>
2024-01-23 17:37:44 +0100

    Initial commit

---
+++ backend/src/main/java/io/paymove/recruitment/RecruitmentTask.java
@@ -0,0 +1,13 @@
+package io.paymove.recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTask {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentTask.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/paymove/recruitment/RecruitmentTaskTests.java
@@ -0,0 +1,13 @@
+package io.paymove.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
